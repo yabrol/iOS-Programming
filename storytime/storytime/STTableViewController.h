d5b399454aa1f29637e2cfcb18c7582a3c9b9de2
Yukti Abrol <yukti.abrol@gmail.com>
2014-09-30 19:09:53 -0400

    added lecture 4 part 1
    playing around with storyboard

---
+++ storytime/storytime/STTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  STTableViewController.h
+//  storytime
+//
+//  Created by Yukti Abrol on 9/30/14.
+//  Copyright (c) 2014 Yukti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STTableViewController : UITableViewController
+
+@end
---
+++ storytime/storytime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  storytime
+//
+//  Created by Yukti Abrol on 9/30/14.
+//  Copyright (c) 2014 Yukti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
