a93b45973b3fa7de0db12fec45eef7cec25c3edf
Yukti Abrol <yukti.abrol@gmail.com>
2014-10-23 16:42:38 -0400

    added lectures and organized into folders

---
+++ Pokedex/Pokedex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pokedex
+//
+//  Created by Yukti Abrol on 10/23/14.
+//  Copyright (c) 2014 Yukti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
