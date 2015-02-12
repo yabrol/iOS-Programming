65a2e22805c25659c1830134367461924ef54275
Yukti <yukti.abrol@gmail.com>
2015-02-11 22:23:24 -0500

    started hw1

---
+++ Spring/HW1/StampChat/StampChat/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  StampChat
+//
+//  Created by Tim Novikoff on 1/25/14.
+//  Copyright (c) 2014 Tim Novikoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
